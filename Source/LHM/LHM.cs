134bcff9ce03f03f1c10719638e8af257648651f
Alex G <oleksandr.goranskiy@uni-jena.de>
2018-09-01 20:34:13 +0200

    Update to b19

--- Source/LHM/LHM.cs
+++ Source/LHM/LHM.cs
@@ -1,109 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using RimWorld;
+using System;
 using System.Linq;
-using System.Text;
-using RimWorld;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace LHM
 {
-    public class LHM_HealOldWoundsProperties : HediffCompProperties
+    public class HediffCompProperties_HealPermanentWounds : HediffCompProperties
     {
-        public LHM_HealOldWoundsProperties()
+        public HediffCompProperties_HealPermanentWounds()
         {
-            this.compClass = typeof(LHM_HealOldWounds);
+            compClass = typeof(HediffComp_HealPermanentWounds);
         }
     }
 
-    public class LHM_HealOldWounds : HediffComp
+    public class HediffComp_HealPermanentWounds : HediffComp
     {
-        private int ticksToNextHeal;
-        private List<string> chronicConditions = new List<string>()           
+        private int ticksToHeal;
+        private List<string> chronicConditions = new List<string>()
         {
-            "BadBack", "Frail", "Cataract", "Blindness", "HearingLoss", "Dementia", "Alzheimers", 
+            "BadBack", "Frail", "Cataract", "Blindness", "HearingLoss", "Dementia", "Alzheimers",
             "Asthma", "HeartArteryBlockage", "Carcinoma", "TraumaSavant", "Cirrhosis",
             "ChemicalDamageSevere", "ChemicalDamageModerate"
         };
 
-        
-        public LHM_HealOldWounds()
-        {
-            //// Load all chronic diseases from game's database
-            //Log.Message("Search for chronic conditions started: ");
-            //foreach (HediffDef hediff in DefDatabase<HediffDef>.AllDefsListForReading)
-            //{
-            //    if (hediff.hediffClass.Name.Equals("HediffWithComps") )
-            //    {
-            //        Log.Message(hediff.defName + " " + hediff.hediffClass + " added");
-            //        chronicConditions.Add(hediff.defName);
-            //    }
-            //}
-        }
-
-        public LHM_HealOldWoundsProperties Props
-        {
-            get
-            {
-                return (LHM_HealOldWoundsProperties)this.props;
-            }
-        }
+        public HediffCompProperties_HealPermanentWounds Props => (HediffCompProperties_HealPermanentWounds)props;
 
         public override void CompPostMake()
         {
             base.CompPostMake();
-            this.ResetTicksToHeal();
+            ResetTicksToHeal();
         }
 
         private void ResetTicksToHeal()
         {
-            this.ticksToNextHeal = Rand.Range(4 * 60000, 6 * 60000); // one day = 60'000 ticks
+            if (Settings.Get().debugHealingSpeed)
+            {
+                ticksToHeal = 3000;
+            }
+            else
+            {
+                ticksToHeal = Rand.Range(4 * 60000, 6 * 60000); // one day = 60'000 ticks
+            }
+
         }
 
         public override void CompPostTick(ref float severityAdjustment)
         {
-            this.ticksToNextHeal--;
-            if (this.ticksToNextHeal <= 0)
+            ticksToHeal--;
+            if (ticksToHeal <= 0)
             {
-                this.TryHealRandomOldWound();
-                this.AffectPawnsAge();
-                this.ResetTicksToHeal();
+                TryHealRandomPermanentWound();
+                AffectPawnsAge();
+                ResetTicksToHeal();
             }
         }
 
-        private void TryHealRandomOldWound()
+        private void TryHealRandomPermanentWound()
         {
-            Hediff hediff;
-            // there should be a way to find chronic diseases without this list for better mod compatibility 
-            if (!(from hd in base.Pawn.health.hediffSet.hediffs
-                  where hd.IsOld() || chronicConditions.Contains(hd.def.defName)
-                  select hd).TryRandomElement(out hediff))
-            {
-                return;
-            }
-            
-            if (hediff != null)
-            {
-                int minHeal = 0.1f;
-                int maxHeal = 0.3f;
-                int meanHeal = (maxHeal - minHeal)/2.0f;
-                float rndHealPercent = meanHeal + Rand.Gaussian() * meanHeal/2.0f; // heal % is normaly distributed between 10 % and 30 %
+            //TODO: there should be a way to find chronic diseases without this list for better mod compatibility
+            var selectHediffsQuery = from hd in base.Pawn.health.hediffSet.hediffs
+                         where hd.IsPermanent() || chronicConditions.Contains(hd.def.defName)
+                         select hd;
+            if (selectHediffsQuery.Any()) {
+                selectHediffsQuery.TryRandomElement(out Hediff hediff);
 
-                float bodyPartMaxHP = hediff.Part.def.GetMaxHealth(hediff.pawn);
-                float healAmount = bodyPartMaxHP * rndHealPercent;
-                if (healAmount < 0.1f) healAmount = 0.1f;
-                if (hediff.Severity - healAmount < 0.1f) base.Pawn.health.hediffSet.hediffs.Remove(hediff);
-                else hediff.Severity -= healAmount;
-              
-            }
+                if (hediff != null)
+                {
+                    float meanHeal = 0.2f;
+                    float rndHealPercent = meanHeal + (Rand.Gaussian() * meanHeal / 2f); // heal % is normaly distributed between 10 % and 30 %
 
-            if (PawnUtility.ShouldSendNotificationAbout(base.Pawn))
-            {
-                Messages.Message("MessageOldWoundHealed".Translate(new object[]
-				{
-					this.parent.Label,
-					base.Pawn.LabelShort,
-					hediff.Label
-				}), MessageTypeDefOf.PositiveEvent);
+                    float bodyPartMaxHP = hediff.Part.def.GetMaxHealth(hediff.pawn);
+                    float healAmount = bodyPartMaxHP * rndHealPercent;
+                    if (healAmount < 0.1f) healAmount = 0.1f;
+                    if (hediff.Severity - healAmount < 0.1f) base.Pawn.health.hediffSet.hediffs.Remove(hediff);
+                    else hediff.Severity -= healAmount;
+
+                }
+
+                if (PawnUtility.ShouldSendNotificationAbout(base.Pawn))
+                {
+                    Messages.Message("MessagePermanentWoundHealed".Translate(parent.LabelCap,
+                        base.Pawn.LabelShort,
+                        hediff.Label), base.Pawn, MessageTypeDefOf.PositiveEvent, true);
+                }
             }
         }
 
@@ -127,7 +109,7 @@ namespace LHM
                     base.Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out num4);
                     string ageAfter = "AgeBiological".Translate(new object[] { biologicalYears, biologicalQuadrums, biologicalDays });
 
-                    if (base.Pawn.IsColonist)
+                    if (base.Pawn.IsColonist && Settings.Get().showAgingMessages)
                     {
                         Messages.Message("MessageAgeReduced".Translate(new object[]
                             {
@@ -135,6 +117,8 @@ namespace LHM
                                 ageBefore,
                                 ageAfter
                             }), MessageTypeDefOf.PositiveEvent);
+                        Messages.Message("MessageAgeReduced".Translate(parent.LabelCap, base.Pawn.LabelShort, ageBefore, ageAfter), 
+                            base.Pawn, MessageTypeDefOf.PositiveEvent, true);
                     }
 
                 }
@@ -145,7 +129,7 @@ namespace LHM
 
             }
             else // if not humanlike then opimal age is the start of the third stage
-            {               
+            {
                 int lifeStage = base.Pawn.ageTracker.CurLifeStageIndex;
                 long startOfThirdStage = (long)(base.Pawn.RaceProps.lifeStageAges[2].minAge * 60 * 60000);
                 long diffFromOptimalAge = base.Pawn.ageTracker.AgeBiologicalTicks - startOfThirdStage;
@@ -159,18 +143,65 @@ namespace LHM
                 }
 
             }
-            
-        }
 
+        }
 
         public override void CompExposeData()
         {
-            Scribe_Values.Look<int>(ref this.ticksToNextHeal, "ticksToHeal", 0, false);
+            Scribe_Values.Look(ref ticksToHeal, "ticksToHeal", 0, false);
         }
 
         public override string CompDebugString()
         {
-            return "ticksToHeal: " + this.ticksToNextHeal;
+            return "ticksToHeal: " + ticksToHeal;
         }
     }
+
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            GetSettings<Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            GetSettings<Settings>().DoWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Luci heals more!";
+        }
+    }
+
+    class Settings : ModSettings
+    {
+        public bool showAgingMessages = false;
+        public bool debugHealingSpeed = false;
+
+        public static Settings Get()
+        {
+            return LoadedModManager.GetMod<LHM.Mod>().GetSettings<Settings>();
+        }
+
+        public void DoWindowContents(Rect wrect)
+        {
+            var options = new Listing_Standard();
+            options.Begin(wrect);
+
+            options.CheckboxLabeled("Show aging messages", checkOn: ref showAgingMessages, tooltip: "Show notification every time age was affected by luci");
+            options.CheckboxLabeled("Debug luci healing", checkOn: ref debugHealingSpeed, tooltip: "Luci heal procs way more often");
+
+            options.End();
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref showAgingMessages, "showAgingMessages", false);
+            Scribe_Values.Look(ref debugHealingSpeed, "debugHealingSpeed", false);
+        }
+    }
+
 }
--- Source/LHM/Properties/AssemblyInfo.cs
+++ Source/LHM/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LHM")]
-[assembly: AssemblyDescription("New definition of luciferium's healOldWound behavior.")]
+[assembly: AssemblyDescription("New definition of luciferium's HealPermanentWounds behavior.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LHM")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1.*")]
+[assembly: AssemblyVersion("1.1.1.*")]
+[assembly: AssemblyFileVersion("1.1.1.*")]
