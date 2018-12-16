761d04937b5da98a953183e8adcc6ff2e6fbb66a
Alex G <oleksandr.goranskiy@uni-jena.de>
2018-12-16 23:42:40 +0100

    hediff selection overhauled
    Still unstable, something causes NRE in TryHealRandomPermanentWound. Usualy with multiple colonists on luci and if all of them have crypto sleep sickness or FoodPoisoning or similar

--- Source/LHM/HediffComp_HealPermanentWounds.cs
+++ Source/LHM/HediffComp_HealPermanentWounds.cs
@@ -9,23 +9,28 @@ namespace LHM
     public class HediffComp_HealPermanentWounds : HediffComp
     {
         private int ticksToHeal;
-        private HashSet<string> chronicConditions = new HashSet<string>()
-        {
-            "Blindness", "TraumaSavant", "Cirrhosis", "ChemicalDamageSevere", "ChemicalDamageModerate"
-        };
 
         public HediffCompProperties_HealPermanentWounds Props => (HediffCompProperties_HealPermanentWounds)props;
 
+        private HashSet<string> chronicConditions = new HashSet<string>()
+        {
+            "ChemicalDamageSevere", "ChemicalDamageModerate"
+        };
+
         public HediffComp_HealPermanentWounds()
         {
+            if(ticksToHeal > 4 * 60000) ResetTicksToHeal();
+
             // Add all hediffs given by HediffGiver_Birthday 
             foreach (HediffGiverSetDef hediffGiverSetDef in DefDatabase<HediffGiverSetDef>.AllDefsListForReading)
             {
                 hediffGiverSetDef.hediffGivers
                     .FindAll(hg => hg.GetType() == typeof(HediffGiver_Birthday))
-                    .ForEach(hg => chronicConditions.Add(hg.hediff.defName));
+                    .ForEach(hg => {
+                        if (hg.hediff.isBad && hg.hediff.everCurableByItem) chronicConditions.Add(hg.hediff.defName);
+                        });
             }
-            Log.Message(string.Join(", ", chronicConditions.ToArray()));
+            Log.Message("Additional chronic conditions that will be healed by luci:\n" + string.Join(", ", chronicConditions.ToArray()));
         }
 
         public override void CompPostMake()
@@ -36,14 +41,8 @@ namespace LHM
 
         private void ResetTicksToHeal()
         {
-            if (Settings.Get().debugHealingSpeed)
-            {
-                ticksToHeal = 3000;
-            }
-            else
-            {
-                ticksToHeal = Rand.Range(4 * 60000, 6 * 60000); // one day = 60'000 ticks
-            }
+            if (Settings.Get().debugHealingSpeed) ticksToHeal = 3000;
+            else ticksToHeal = Rand.Range(4 * 60000, 6 * 60000); // one day = 60'000 ticks
         }
 
         public override void CompPostTick(ref float severityAdjustment)
@@ -60,7 +59,7 @@ namespace LHM
 
         private void TryHealRandomPermanentWound()
         {
-            var selectHediffsQuery = from hd in base.Pawn.health.hediffSet.hediffs
+            var selectHediffsQuery = from hd in Pawn.health.hediffSet.hediffs
                                      where hd.IsPermanent() || chronicConditions.Contains(hd.def.defName)
                                      select hd;
             if (selectHediffsQuery.Any())
@@ -73,74 +72,84 @@ namespace LHM
                     float rndHealPercent = meanHeal + (Rand.Gaussian() * meanHeal / 2f); // heal % is normaly distributed between 10 % and 30 %
 
                     float bodyPartMaxHP = hediff.Part.def.GetMaxHealth(hediff.pawn);
-                    float healAmount = bodyPartMaxHP * rndHealPercent;
-                    if (healAmount < 0.1f) healAmount = 0.1f;
-                    if (hediff.Severity - healAmount < 0.1f) base.Pawn.health.hediffSet.hediffs.Remove(hediff);
+                    float rawHealAmount = bodyPartMaxHP * rndHealPercent;
+                    float healAmount = (rawHealAmount < 0.1f) ? 0.1f : rawHealAmount;
+
+                    if (hediff.Severity - healAmount < 0.1f) HandleLowSeverity(hediff);
                     else hediff.Severity -= healAmount;
                 }
+            }
+        }
 
-                if (PawnUtility.ShouldSendNotificationAbout(base.Pawn))
-                {
-                    Messages.Message("MessagePermanentWoundHealed".Translate(parent.LabelCap,
-                        base.Pawn.LabelShort,
-                        hediff.Label), base.Pawn, MessageTypeDefOf.PositiveEvent, true);
-                }
+        private void HandleLowSeverity(Hediff hediff)
+        {
+            string hediffName = hediff.def.defName;
+
+            if (hediff.IsPermanent()) hediff.Severity = 0f;
+            else Pawn.health.RemoveHediff(hediff);
+
+            if (PawnUtility.ShouldSendNotificationAbout(Pawn))
+            {
+                Messages.Message("MessagePermanentWoundHealed".Translate(
+                        parent.LabelCap,
+                        Pawn.LabelShort, hediffName,
+                        Pawn.Named("PAWN")
+                    ),
+                    Pawn, MessageTypeDefOf.PositiveEvent, true
+                );
             }
         }
 
         private void AffectPawnsAge()
         {
-            if (base.Pawn.RaceProps.Humanlike)
+            if (Pawn.RaceProps.Humanlike)
             {
-                if (base.Pawn.ageTracker.AgeBiologicalYears > 25)
-                {
-                    int biologicalYears;
-                    int biologicalQuadrums;
-                    int biologicalDays;
-                    float biologicalHours;
-
-                    base.Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out biologicalHours);
-
-                    string ageBefore = "AgeBiological".Translate(new object[] { biologicalYears, biologicalQuadrums, biologicalDays });
-                    long diffFromOptimalAge = base.Pawn.ageTracker.AgeBiologicalTicks - 25 * 60 * 60000;
-                    base.Pawn.ageTracker.AgeBiologicalTicks -= (long)(diffFromOptimalAge * 0.05f);
-
-                    base.Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out biologicalHours);
-                    string ageAfter = "AgeBiological".Translate(new object[] { biologicalYears, biologicalQuadrums, biologicalDays });
-
-                    if (Pawn.IsColonist && Settings.Get().showAgingMessages)
-                    {
-                        Messages.Message("MessageAgeReduced".Translate(new object[]
-                            {
-                                base.Pawn.LabelShort,
-                                ageBefore,
-                                ageAfter
-                            }), MessageTypeDefOf.PositiveEvent);
-                        Messages.Message("MessageAgeReduced".Translate(parent.LabelCap, base.Pawn.LabelShort, ageBefore, ageAfter),
-                            base.Pawn, MessageTypeDefOf.PositiveEvent, true);
-                    }
-                }
-                else if (base.Pawn.ageTracker.AgeBiologicalYears < 25) // if 25 do nothing, if younger that 25, that mature faster towards 25
-                {
-                    base.Pawn.ageTracker.AgeBiologicalTicks += (long)(15 * 60000); // get one quadrum older
-                }
-
+                if (Pawn.ageTracker.AgeBiologicalYears > 25) ReduceAgeOfHumanlike();
+                else if (Pawn.ageTracker.AgeBiologicalYears < 25) Pawn.ageTracker.AgeBiologicalTicks += (long)(15 * 60000); // get one quadrum older
             }
-            else // if not humanlike then opimal age is the start of the third stage
+            else // if not humanlike then optimal age is the start of the third stage
             {
-                int lifeStage = base.Pawn.ageTracker.CurLifeStageIndex;
-                long startOfThirdStage = (long)(base.Pawn.RaceProps.lifeStageAges[2].minAge * 60 * 60000);
-                long diffFromOptimalAge = base.Pawn.ageTracker.AgeBiologicalTicks - startOfThirdStage;
-                if (lifeStage >= 3) // then need to become younger
+                int lifeStage = Pawn.ageTracker.CurLifeStageIndex;
+                long startOfThirdStage = (long)(Pawn.RaceProps.lifeStageAges[2].minAge * 60 * 60000);
+                long diffFromOptimalAge = Pawn.ageTracker.AgeBiologicalTicks - startOfThirdStage;
+                if (lifeStage >= 3 && diffFromOptimalAge > 0) // then need to become younger
                 {
-                    base.Pawn.ageTracker.AgeBiologicalTicks -= (long)(diffFromOptimalAge * 0.05f);
+                    Pawn.ageTracker.AgeBiologicalTicks -= (long)(diffFromOptimalAge * 0.05f);
                 }
                 else // in that case mature faster towards 3rd stage
                 {
-                    base.Pawn.ageTracker.AgeBiologicalTicks += (long)(5 * 60000); // get 5 days older
+                    Pawn.ageTracker.AgeBiologicalTicks += (long)(5 * 60000); // get 5 days older
                 }
             }
 
+        }
+
+        private void ReduceAgeOfHumanlike()
+        {
+            int biologicalYears;
+            int biologicalQuadrums;
+            int biologicalDays;
+            float biologicalHours;
+
+            Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out biologicalHours);
+
+            string ageBefore = "AgeBiological".Translate(biologicalYears, biologicalQuadrums, biologicalDays);
+            long diffFromOptimalAge = Pawn.ageTracker.AgeBiologicalTicks - 25 * 60 * 60000;
+            Pawn.ageTracker.AgeBiologicalTicks -= (long)(diffFromOptimalAge * 0.05f);
+
+            Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out biologicalHours);
+            string ageAfter = "AgeBiological".Translate(biologicalYears, biologicalQuadrums, biologicalDays);
+
+            if (Pawn.IsColonist && Settings.Get().showAgingMessages)
+            {
+                Messages.Message("MessageAgeReduced".Translate(
+                        Pawn.LabelShort,
+                        ageBefore,
+                        ageAfter
+                    ),
+                    MessageTypeDefOf.PositiveEvent, true
+                );
+            }
         }
 
         public override void CompExposeData()
