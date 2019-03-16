28227608a738f5d6d4b1a8da89a944feb5c0fe62
Alex G <oleksandr.goranskiy@uni-jena.de>
2019-03-16 17:06:43 +0100

    Bugfix: some reported issues solved
    * Null reference in TryHealRandomPermanentWound fixed
    * Hediffs are now healed 10 to 30 % at a time as expected, and not at once
    * Better compatibility with other mods

--- Source/LHM/HediffComp_HealPermanentWounds.cs
+++ Source/LHM/HediffComp_HealPermanentWounds.cs
@@ -1,6 +1,5 @@
 ﻿using RimWorld;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using Verse;
 
@@ -8,18 +7,19 @@ namespace LHM
 {
     public class HediffComp_HealPermanentWounds : HediffComp
     {
+        private const int TicksInDay = 60000;
         private int ticksToHeal;
 
         public HediffCompProperties_HealPermanentWounds Props => (HediffCompProperties_HealPermanentWounds)props;
 
-        private HashSet<string> chronicConditions = new HashSet<string>()
+        public HashSet<string> ChronicConditions { get; set; } = new HashSet<string>()
         {
-            "ChemicalDamageSevere", "ChemicalDamageModerate"
+            "TraumaSavant", "ChemicalDamageSevere", "ChemicalDamageModerate"
         };
 
         public HediffComp_HealPermanentWounds()
         {
-            if(ticksToHeal > 4 * 60000) ResetTicksToHeal();
+            if(ticksToHeal > 4 * TicksInDay) ResetTicksToHeal();
 
             // Add all hediffs given by HediffGiver_Birthday 
             foreach (HediffGiverSetDef hediffGiverSetDef in DefDatabase<HediffGiverSetDef>.AllDefsListForReading)
@@ -27,10 +27,10 @@ namespace LHM
                 hediffGiverSetDef.hediffGivers
                     .FindAll(hg => hg.GetType() == typeof(HediffGiver_Birthday))
                     .ForEach(hg => {
-                        if (hg.hediff.isBad && hg.hediff.everCurableByItem) chronicConditions.Add(hg.hediff.defName);
+                        if (hg.hediff.isBad && hg.hediff.everCurableByItem) ChronicConditions.Add(hg.hediff.defName);
                         });
             }
-            Log.Message("Additional chronic conditions that will be healed by luci:\n" + string.Join(", ", chronicConditions.ToArray()));
+            Log.Message("Additional chronic conditions that will be healed by luci:\n" + string.Join(", ", ChronicConditions.ToArray()));
         }
 
         public override void CompPostMake()
@@ -41,14 +41,14 @@ namespace LHM
 
         private void ResetTicksToHeal()
         {
-            if (Settings.Get().debugHealingSpeed) ticksToHeal = 3000;
-            else ticksToHeal = Rand.Range(4 * 60000, 6 * 60000); // one day = 60'000 ticks
+            // next heal event will happen after an hour in the debug mode or after 4 to 6 days (uniformly distributed) normaly
+            ticksToHeal = Settings.Get().debugHealingSpeed ? 2500 : Rand.Range(4 * TicksInDay, 6 * TicksInDay); 
         }
 
         public override void CompPostTick(ref float severityAdjustment)
         {
             ticksToHeal--;
-            if (ticksToHeal >= 4 * 60000) ResetTicksToHeal();
+            if (ticksToHeal >= 6 * TicksInDay) ResetTicksToHeal();
             else if (ticksToHeal <= 0)
             {
                 TryHealRandomPermanentWound();
@@ -56,35 +56,29 @@ namespace LHM
                 ResetTicksToHeal();
             }
         }
-
+        
         private void TryHealRandomPermanentWound()
         {
             var selectHediffsQuery = from hd in Pawn.health.hediffSet.hediffs
-                                     where hd.IsPermanent() || chronicConditions.Contains(hd.def.defName)
+                                     where hd.IsPermanent() || ChronicConditions.Contains(hd.def.defName)
                                      select hd;
-            if (selectHediffsQuery.Any())
-            {
-                selectHediffsQuery.TryRandomElement(out Hediff hediff);
 
-                if (hediff != null)
-                {
-                    float meanHeal = 0.2f;
-                    float rndHealPercent = meanHeal + (Rand.Gaussian() * meanHeal / 2f); // heal % is normaly distributed between 10 % and 30 %
+            if (selectHediffsQuery.TryRandomElement(out Hediff hediff))  
+            {                
+                float meanHeal = 0.2f;
+                float rndHealPercent = meanHeal + (Rand.Gaussian() * meanHeal / 2f); // heal % is normaly distributed between 10 % and 30 %
 
-                    float bodyPartMaxHP = hediff.Part.def.GetMaxHealth(hediff.pawn);
-                    float rawHealAmount = bodyPartMaxHP * rndHealPercent;
-                    float healAmount = (rawHealAmount < 0.1f) ? 0.1f : rawHealAmount;
+                float bodyPartMaxHP = hediff.Part.def.GetMaxHealth(hediff.pawn);
+                float rawHealAmount = hediff.IsPermanent() ? bodyPartMaxHP * rndHealPercent : rndHealPercent;
+                float healAmount = (rawHealAmount < 0.1f) ? 0.1f : rawHealAmount;
 
-                    if (hediff.Severity - healAmount < 0.1f) HandleLowSeverity(hediff);
-                    else hediff.Severity -= healAmount;
-                }
+                if (hediff.Severity - healAmount < 0.1f) HandleLowSeverity(hediff);
+                else hediff.Severity -= healAmount;
             }
         }
 
         private void HandleLowSeverity(Hediff hediff)
         {
-            string hediffName = hediff.def.defName;
-
             if (hediff.IsPermanent()) hediff.Severity = 0f;
             else Pawn.health.RemoveHediff(hediff);
 
@@ -92,7 +86,8 @@ namespace LHM
             {
                 Messages.Message("MessagePermanentWoundHealed".Translate(
                         parent.LabelCap,
-                        Pawn.LabelShort, hediffName,
+                        Pawn.LabelShort,
+                        hediff.Label,
                         Pawn.Named("PAWN")
                     ),
                     Pawn, MessageTypeDefOf.PositiveEvent, true
@@ -105,12 +100,12 @@ namespace LHM
             if (Pawn.RaceProps.Humanlike)
             {
                 if (Pawn.ageTracker.AgeBiologicalYears > 25) ReduceAgeOfHumanlike();
-                else if (Pawn.ageTracker.AgeBiologicalYears < 25) Pawn.ageTracker.AgeBiologicalTicks += (long)(15 * 60000); // get one quadrum older
+                else if (Pawn.ageTracker.AgeBiologicalYears < 25) Pawn.ageTracker.AgeBiologicalTicks += (long)(15 * TicksInDay); // get one quadrum older
             }
             else // if not humanlike then optimal age is the start of the third stage
             {
                 int lifeStage = Pawn.ageTracker.CurLifeStageIndex;
-                long startOfThirdStage = (long)(Pawn.RaceProps.lifeStageAges[2].minAge * 60 * 60000);
+                long startOfThirdStage = (long)(Pawn.RaceProps.lifeStageAges[2].minAge * 60 * TicksInDay);
                 long diffFromOptimalAge = Pawn.ageTracker.AgeBiologicalTicks - startOfThirdStage;
                 if (lifeStage >= 3 && diffFromOptimalAge > 0) // then need to become younger
                 {
@@ -118,7 +113,7 @@ namespace LHM
                 }
                 else // in that case mature faster towards 3rd stage
                 {
-                    Pawn.ageTracker.AgeBiologicalTicks += (long)(5 * 60000); // get 5 days older
+                    Pawn.ageTracker.AgeBiologicalTicks += (long)(5 * TicksInDay); // get 5 days older
                 }
             }
 
@@ -134,7 +129,7 @@ namespace LHM
             Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out biologicalHours);
 
             string ageBefore = "AgeBiological".Translate(biologicalYears, biologicalQuadrums, biologicalDays);
-            long diffFromOptimalAge = Pawn.ageTracker.AgeBiologicalTicks - 25 * 60 * 60000;
+            long diffFromOptimalAge = Pawn.ageTracker.AgeBiologicalTicks - (25 * 60 * TicksInDay);
             Pawn.ageTracker.AgeBiologicalTicks -= (long)(diffFromOptimalAge * 0.05f);
 
             Pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out biologicalYears, out biologicalQuadrums, out biologicalDays, out biologicalHours);
