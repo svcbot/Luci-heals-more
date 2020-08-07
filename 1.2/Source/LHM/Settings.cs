88c69b5e2961a0da91fa07c5815985bba8228ed5
Alex G <oleksandr.gor@gmail.com>
2020-08-07 21:48:31 +0200

    update for rimworld 1.2

---
+++ 1.2/Source/LHM/Settings.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace LHM
+{
+    class Settings : ModSettings
+    {
+        public bool showAgingMessages = false;
+        public bool debugHealingSpeed = false;
+
+        public static Settings Get()
+        {
+            return LoadedModManager.GetMod<LHM.Mod>().GetSettings<Settings>();
+        }
+
+        public void DoWindowContents(Rect wrect)
+        {
+            var options = new Listing_Standard();
+            options.Begin(wrect);
+
+            options.CheckboxLabeled("Show aging messages", checkOn: ref showAgingMessages, tooltip: "Show notification every time age was affected by luci");
+            options.CheckboxLabeled("Debug luci healing", checkOn: ref debugHealingSpeed, tooltip: "Luci heal procs way more often");
+
+            options.End();
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref showAgingMessages, "showAgingMessages", false);
+#if DEBUG
+            Scribe_Values.Look(ref debugHealingSpeed, "debugHealingSpeed", false);
+#endif
+        }
+    }
+
+}
