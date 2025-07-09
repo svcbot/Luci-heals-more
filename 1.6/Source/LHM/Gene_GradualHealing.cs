ec6d592d15e8a382c268c325acf42fe59b73a3cd
Alex G <oleksandr.gor@gmail.com>
2025-07-09 17:32:06 +0200

    Update to game version 1.6 (#20)

---
+++ 1.6/Source/LHM/Gene_GradualHealing.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace LHM
+{
+
+	public class Gene_GradualHealing : Gene
+	{
+		private int ticksToHeal;
+
+		public override void PostAdd()
+		{
+			base.PostAdd();
+			ResetInterval();
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+			ticksToHeal--;
+			if (ticksToHeal <= 0 || ticksToHeal > GenDate.TicksPerHour * 4)
+			{
+				HediffComp_LuciferiumHeal.TryHealRandomPermanentWound(pawn, LabelCap);
+				ResetInterval();
+			}
+		}
+
+		private void ResetInterval()
+		{
+			ticksToHeal = Settings.Get().EnableDebugHealingSpeed
+				? GenDate.TicksPerHour / 4
+				: GenDate.TicksPerHour * 4;
+		}
+
+		public override IEnumerable<Gizmo> GetGizmos()
+		{
+			if (DebugSettings.ShowDevGizmos)
+			{
+				yield return new Command_Action
+				{
+					defaultLabel = "DEV: Heal permanent wound",
+					action = delegate
+					{
+						HediffComp_HealPermanentWounds.TryHealRandomPermanentWound(pawn, LabelCap);
+						ResetInterval();
+					}
+				};
+			}
+		}
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref ticksToHeal, "ticksToHeal", 0);
+		}
+	}
+}
