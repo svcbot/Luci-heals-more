94b1d190d053ee6eae498fc425a1b2192ad02fe6
Alex G. <oleksandr.gor@gmail.com>
2025-09-21 13:59:10 +0200

    Fix age increase and use optimal age from settings everywhere

--- 1.6/Source/LHM/Settings.cs
+++ 1.6/Source/LHM/Settings.cs
@@ -58,12 +58,12 @@ namespace LHM
                 options.Label("Hunger rate treshold: " + (int)HungerRateTreshold);
                 hungerRateTreshold = options.Slider(hungerRateTreshold, 100f, 200f);
 
-                options.Gap();
-                options.Label("Healing power");
-                options.RadioButton("Vanilla", true, tooltip: "Comparable to vanilla logic. A full limb will take 15 to 30 days to regrow.");
-                options.RadioButton("More healing", false, tooltip: "3x healing power compared to vanila. A full limb will take 5 to 15 days to regrow.");
-                options.RadioButton("OP", false, tooltip: "OP healing, not recomended. A full limb will take 1 to 3 days to regrow.");
-                options.RadioButton("God mode", false, tooltip: "x32 healing, useful for debug purposes. A full limb will take a few hours.");
+                //options.Gap();
+                //options.Label("Healing power");
+                //options.RadioButton("Vanilla", true, tooltip: "Comparable to vanilla logic. A full limb will take 15 to 30 days to regrow.");
+                //options.RadioButton("More healing", false, tooltip: "3x healing power compared to vanila. A full limb will take 5 to 15 days to regrow.");
+                //options.RadioButton("OP", false, tooltip: "OP healing, not recomended. A full limb will take 1 to 3 days to regrow.");
+                //options.RadioButton("God mode", false, tooltip: "x32 healing, useful for debug purposes. A full limb will take a few hours.");
 
             }
 
